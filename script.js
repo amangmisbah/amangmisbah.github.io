fb1d38934d3d439495f7b6fd73d349b27092cf7a
amangmisbah <amangmisbah5@gmal.com>
2021-03-12 21:32:47 +0700

    menambahkan fitur contact form

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-var btn = $('#button');
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.alert');
 
-$(window).scroll(function () {
-    if ($(window).scrollTop() > 300) {
-        btn.addClass('show');
-    } else {
-        btn.removeClass('show');
-    }
-});
 
-btn.on('click', function (e) {
+const scriptURL =
+    'https://script.google.com/macros/s/AKfycbz_bqPHVthA_nu2v5SuGdt-mNIjhXWz9Aq0WkOGvAQafvtuZlkCk7Tfw_3TzxnOAnFB/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
     e.preventDefault();
-    $('html, body').animate({
-        scrollTop: 0
-    }, '300');
+    //ketika tombol submit diklik
+    //tampilkan tombol loading
+    btnLoading.classList.toggle('d-none');
+    btnKirim.classList.toggle('d-none');
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
 });
